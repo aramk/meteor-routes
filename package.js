776f6881ef8d7d65462a28858c86d272a5d89229
Aram Kocharyan <akarmenia@gmail.com>
2015-04-30 12:05:23 +1000

    Added version for dependency.

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function (api) {
   api.use([
     'coffeescript',
     'underscore',
-    'reactive-var',
+    'reactive-var@1.0.5',
     'aramk:q@1.0.1',
     'aramk:utility@0.8.3',
     'iron:router@1.0.7'
