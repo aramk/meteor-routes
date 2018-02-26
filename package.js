ef7ffe7ed1ca4156f22f5178331ca1a6921cf9cb
Aram Kocharyan <akocharyan@fb.com>
2018-02-26 01:01:33 -0800

    Meteor. 1.6.1 support

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:routes',
-  version: '0.3.3',
+  version: '1.0.0',
   summary: 'Convenient utilities for setting up routes for a Meteor app.',
   git: 'https://github.com/aramk/meteor-routes.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.2.0.1');
+  api.versionsFrom('METEOR@1.6.1');
   api.use([
     'coffeescript',
     'underscore',
     'reactive-var@1.0.5',
     'aramk:q@1.0.1',
     'iron:router@1.0.13',
-    'urbanetic:utility@1.2.0'
+    'urbanetic:utility@2.0.0'
   ], 'client');
   api.imply('iron:router');
   api.export('Routes', 'client');
